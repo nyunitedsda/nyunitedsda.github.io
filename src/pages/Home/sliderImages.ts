6c35e18d006ac4a4e87e306a27e590156b741c09
Nial Hope <nyevincy@gmail.com>
2025-05-29 03:35:55 -0400

    fix: replace dynamic imports with static imports for slider images

--- src/pages/Home/sliderImages.ts
+++ src/pages/Home/sliderImages.ts
@@ -1,35 +1,57 @@
-const elderImage = (await import("../../assets/webp-img/elder.webp")).default;
-const familyImage = (await import("../../assets/webp-img/family.webp")).default;
-const family2Image = (await import("../../assets/webp-img/family2.webp"))
-	.default;
-const family3Image = (await import("../../assets/img/family3.jpg")).default;
-const family4Image = (await import("../../assets/img/family4.jpg")).default;
-const family5Image = (await import("../../assets/img/family5.jpg")).default;
-const givingGiftImage = (await import("../../assets/img/giving-gift.jpg"))
-	.default;
-const groupPrayImage = (await import("../../assets/webp-img/group-pray.webp"))
-	.default;
-const membersEmbraceImage = (
-	await import("../../assets/img/members-embrace.jpg")
-).default;
-const membersEmbrace1Image = (
-	await import("../../assets/img/members-embrace1.jpg")
-).default;
-const membersEmbrace2Image = (
-	await import("../../assets/img/members-embrace2.jpg")
-).default;
-const membersImage = (await import("../../assets/img/members.jpg")).default;
-const members3Image = (await import("../../assets/img/members3.jpg")).default;
-const members4Image = (await import("../../assets/img/members4.jpg")).default;
-const members6Image = (await import("../../assets/img/members6.jpg")).default;
-const singingSisterImage = (await import("../../assets/img/singing-sister.jpg"))
-	.default;
-const sisterPhotographyImage = (
-	await import("../../assets/img/sister-photography.jpg")
-).default;
-const youthsImage = (await import("../../assets/img/youths.jpg")).default;
-const youths2Image = (await import("../../assets/img/youths2.jpg")).default;
-const youths3Image = (await import("../../assets/img/youths3.jpg")).default;
+import elderImage from "../../assets/webp-img/elder.webp";
+import familyImage from "../../assets/webp-img/family.webp";
+import family2Image from "../../assets/webp-img/family2.webp";
+
+import family3Image from "../../assets/img/family3.jpg";
+import family4Image from "../../assets/img/family4.jpg";
+import family5Image from "../../assets/img/family5.jpg";
+import givingGiftImage from "../../assets/img/giving-gift.jpg";
+import groupPrayImage from "../../assets/webp-img/group-pray.webp";
+import membersEmbraceImage from "../../assets/img/members-embrace.jpg";
+import membersEmbrace1Image from "../../assets/img/members-embrace1.jpg";
+import membersEmbrace2Image from "../../assets/img/members-embrace2.jpg";
+import membersImage from "../../assets/img/members.jpg";
+import members3Image from "../../assets/img/members3.jpg";
+import members4Image from "../../assets/img/members4.jpg";
+import members6Image from "../../assets/img/members6.jpg";
+import singingSisterImage from "../../assets/img/singing-sister.jpg";
+import sisterPhotographyImage from "../../assets/img/sister-photography.jpg";
+import youthsImage from "../../assets/img/youths.jpg";
+import youths2Image from "../../assets/img/youths2.jpg";
+import youths3Image from "../../assets/img/youths3.jpg";
+
+// const elderImage = (await import("../../assets/webp-img/elder.webp")).default;
+// const familyImage = (await import("../../assets/webp-img/family.webp")).default;
+// const family2Image = (await import("../../assets/webp-img/family2.webp"))
+// 	.default;
+// const family3Image = (await import("../../assets/img/family3.jpg")).default;
+// const family4Image = (await import("../../assets/img/family4.jpg")).default;
+// const family5Image = (await import("../../assets/img/family5.jpg")).default;
+// const givingGiftImage = (await import("../../assets/img/giving-gift.jpg"))
+// 	.default;
+// const groupPrayImage = (await import("../../assets/webp-img/group-pray.webp"))
+// 	.default;
+// const membersEmbraceImage = (
+// 	await import("../../assets/img/members-embrace.jpg")
+// ).default;
+// const membersEmbrace1Image = (
+// 	await import("../../assets/img/members-embrace1.jpg")
+// ).default;
+// const membersEmbrace2Image = (
+// 	await import("../../assets/img/members-embrace2.jpg")
+// ).default;
+// const membersImage = (await import("../../assets/img/members.jpg")).default;
+// const members3Image = (await import("../../assets/img/members3.jpg")).default;
+// const members4Image = (await import("../../assets/img/members4.jpg")).default;
+// const members6Image = (await import("../../assets/img/members6.jpg")).default;
+// const singingSisterImage = (await import("../../assets/img/singing-sister.jpg"))
+// 	.default;
+// const sisterPhotographyImage = (
+// 	await import("../../assets/img/sister-photography.jpg")
+// ).default;
+// const youthsImage = (await import("../../assets/img/youths.jpg")).default;
+// const youths2Image = (await import("../../assets/img/youths2.jpg")).default;
+// const youths3Image = (await import("../../assets/img/youths3.jpg")).default;
 
 export default [
 	{ src: elderImage, alt: "Elder" },
