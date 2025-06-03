1b28e411afca91b7eaa48f3e9b4c234b26f939e3
Nial Hope <nyevincy@gmail.com>
2025-06-03 12:03:45 -0400

    feat: refactor MenuButton component for improved menu handling and responsiveness

--- src/components/Buttons/MenuButton.tsx
+++ src/components/Buttons/MenuButton.tsx
@@ -7,12 +7,12 @@ import Paper from "@mui/material/Paper";
 import Popper from "@mui/material/Popper";
 import type { SxProps, Theme } from "@mui/material/styles";
 import {
-	type FC,
-	type MouseEvent,
-	useCallback,
-	useMemo,
-	useRef,
-	useState,
+  type FC,
+  type MouseEvent,
+  useCallback,
+  useMemo,
+  useRef,
+  useState,
 } from "react";
 import { useNavigate } from "react-router";
 import { menuItemStyles } from "../Header/styles";
@@ -23,89 +23,96 @@ const { activeBtnSx, buttonSx } = menuButtonStyles;
 const { activeMenuItemSx, menuItemSx } = menuItemStyles;
 
 const MenuButton: FC<MenuButtonProps> = (props) => {
-	const { isActive, path, children, buttonProps, menuItems } = props;
+  const { isActive, path, children, buttonProps, menuItems } = props;
 
-	const [isMenuVisible, setIsMenuVisible] = useState<boolean>(false);
-	const buttonRef = useRef<HTMLButtonElement>(null);
-	const navigate = useNavigate();
+  const [isMenuVisible, setIsMenuVisible] = useState<boolean>(false);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const navigate = useNavigate();
 
-	const isActiveButton = useMemo((): boolean => {
-		return menuItems && menuItems?.length > 0
-			? menuItems.some((i) => isActive(i.path))
-			: isActive(path);
-	}, [menuItems, path]);
+  const isActiveButton = useMemo((): boolean => {
+    return menuItems && menuItems?.length > 0
+      ? menuItems.some((i) => isActive(i.path))
+      : isActive(path);
+  }, [menuItems, path]);
 
-	const handleClose = useCallback(() => {
-		setIsMenuVisible(false);
-	}, []);
+  const handleMenuClose = useCallback(() => {
+    setIsMenuVisible(false);
+  }, []);
 
-	const handleMenuItemSelect = useCallback(
-		(_event: MouseEvent<HTMLLIElement>, url: string) => {
-			navigate(url);
-			handleClose();
-		},
-		[handleClose],
-	);
+  const handleMenuOpen = useCallback(() => {
+    setIsMenuVisible(true);
+  }, []);
 
-	return (
-		<>
-			<Button
-				{...buttonProps}
-				href={path}
-				ref={buttonRef}
-				sx={
-					{
-						...buttonSx,
-						...(isActiveButton ? activeBtnSx : {}),
-					} as SxProps<Theme>
-				}
-			>
-				{children} {/* name */}
-			</Button>
-			{isMenuVisible && (
-				<Popper
-					sx={{ zIndex: 1 }}
-					open={isMenuVisible}
-					anchorEl={buttonRef.current}
-					role={undefined}
-					transition
-					disablePortal
-				>
-					{({ TransitionProps, placement }) => (
-						<Grow
-							{...TransitionProps}
-							style={{
-								transformOrigin:
-									placement === "bottom" ? "center top" : "center bottom",
-							}}
-						>
-							<Paper>
-								<ClickAwayListener onClickAway={handleClose}>
-									<MenuList id="split-button-menu" autoFocusItem>
-										{menuItems?.map((i) => (
-											<MenuItem
-												key={i.name}
-												selected={isActive(i.path)}
-												onClick={(event) => handleMenuItemSelect(event, i.path)}
-												sx={
-													{
-														...menuItemSx,
-														...(isActive(i.path) ? activeMenuItemSx : {}),
-													} as SxProps<Theme>
-												}
-											>
-												{i.name}
-											</MenuItem>
-										))}
-									</MenuList>
-								</ClickAwayListener>
-							</Paper>
-						</Grow>
-					)}
-				</Popper>
-			)}
-		</>
-	);
+  const handleMenuItemSelect = useCallback(
+    (_event: MouseEvent<HTMLLIElement>, url: string) => {
+      navigate(url);
+      handleMenuClose();
+    },
+    [handleMenuClose],
+  );
+
+  return (
+    <>
+      <Button
+        {...buttonProps}
+        onClick={menuItems ? handleMenuOpen : buttonProps?.onClick}
+        href={!menuItems ? path : undefined}
+        ref={buttonRef}
+        sx={
+          {
+            ...buttonSx,
+            ...(isActiveButton ? activeBtnSx : {}),
+          } as SxProps<Theme>
+        }
+      >
+        {children} {/* name */}
+      </Button>
+      {
+      isMenuVisible && menuItems && (
+        <Popper
+          sx={{ zIndex: 1 }}
+          open={isMenuVisible}
+          anchorEl={buttonRef.current}
+          role={undefined}
+          placement="bottom-end"
+          transition
+          disablePortal
+        >
+          {({ TransitionProps, placement }) => (
+            <Grow
+              {...TransitionProps}
+              style={{
+                transformOrigin:
+                  placement === "bottom" ? "center top" : "center bottom",
+              }}
+            >
+              <Paper>
+                <ClickAwayListener onClickAway={handleMenuClose}>
+                  <MenuList id="split-button-menu" autoFocusItem>
+                    {menuItems?.map((i) => (
+                      <MenuItem
+                        key={i.name}
+                        selected={isActive(i.path)}
+                        onClick={(event) => handleMenuItemSelect(event, i.path)}
+                        sx={
+                          {
+                            ...menuItemSx,
+                            ...(isActive(i.path) ? activeMenuItemSx : {}),
+                          } as SxProps<Theme>
+                        }
+                      >
+                        {i.name}
+                      </MenuItem>
+                    ))}
+                  </MenuList>
+                </ClickAwayListener>
+              </Paper>
+            </Grow>
+          )}
+        </Popper>
+      )}
+    </>
+  );
 };
 
 export default MenuButton;
