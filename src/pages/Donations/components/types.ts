2a5cbfc82ad69b999abc2a7c8e601aeea00c4fc9
Nial Hope <nyevincy@gmail.com>
2025-07-27 19:38:43 -0400

    feat: remove DonationItem component and its associated tests and types

--- src/pages/Donations/components/types.ts
+++
@@ -1,9 +0,0 @@
-import type { SxProps, Theme } from "@mui/material/styles";
-
-export interface DonationItemProps {
-	sx?: SxProps<Theme>;
-	title: string;
-	subtitle: string;
-	onEdit?: () => void;
-	onDelete?: () => void;
-}
