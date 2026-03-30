7e6d8cc9f5724000eb603e2c1c907fd362023847
Nial Hope <nyevincy@gmail.com>
2026-03-29 20:44:22 -0400

    feat: Check Services date format in admin

--- src/components/Footer/components/FooterServiceTime.tsx
+++ src/components/Footer/components/FooterServiceTime.tsx
@@ -1,9 +1,10 @@
+import type { ServiceDT } from "@/api";
 import { FooterSegment, FooterSegmentSkeleton } from "@components/Footer";
 import Typography from "@mui/material/Typography";
 import { CONTACT_CONSTANT } from "@pages/Contact";
+import dayjs from "dayjs";
 import type { FC } from "react";
 import { useLoaderData } from "react-router";
-import type { ServiceDT } from "@/api";
 
 const FooterServiceTime: FC = () => {
 	const { services, isLoading } = useLoaderData();
@@ -14,7 +15,7 @@ const FooterServiceTime: FC = () => {
 			) : (
 				services?.map((i: ServiceDT) => (
 					<Typography key={i.title} variant="body2">
-						<strong>{`${i.title}: `}</strong> {i.time}
+						<strong>{`${i.title}: `}</strong> {dayjs(i.time).isValid() ? dayjs(i.time).format('h:mm a') : i.time}
 					</Typography>
 				))
 			)}
--- src/pages/Contact/components/ServiceTimes.tsx
+++ src/pages/Contact/components/ServiceTimes.tsx
@@ -1,3 +1,4 @@
+import type { ServiceDT } from "@/api";
 import { useEntityList } from "@hooks/api";
 import {
 	CONTACT_CONSTANT,
@@ -5,8 +6,8 @@ import {
 	ContactSectionSkeleton,
 } from "@pages/Contact";
 import { NoteSection } from "@pages/Home";
+import dayjs from "dayjs";
 import type { FC } from "react";
-import type { ServiceDT } from "@/api";
 
 const ServiceTimes: FC = () => {
 	const { data, error, isLoading } = useEntityList<ServiceDT>("services");
@@ -16,7 +17,11 @@ const ServiceTimes: FC = () => {
 			{!isLoading && !error && (
 				<ContactSection title={CONTACT_CONSTANT.SERVICES}>
 					{data?.map((i) => (
-						<NoteSection content={i.time} key={i.title} title={`${i.title}:`} />
+						<NoteSection
+							content={dayjs(i.time).isValid() ? dayjs(i.time).format('h:mm a') : i.time}
+							key={i.title}
+							title={`${i.title}:`}
+						/>
 					))}
 				</ContactSection>
 			)}
